fbe1d03ca58fdcd5a3eb0839c04fe557b71d7840
LurdesMg <lurdesxi07@gmail.com>
2023-05-24 10:46:57 +0200

    fix hero

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 /* SECCIÓN DE IMPORT */
 import '../styles/Reset.scss';
-import '../styles/App.scss';
 import '../styles/Mixins.scss';
+import '../styles/App.scss';
 
 import { useState, useEffect } from 'react';
 import ls from '../service/localStorage';
